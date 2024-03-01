befdba70ec454920999ad59d1f1a0b3f8dc04977
Carlos Delgado <carlos.andres.delgado@correounivalle.edu.co>
2024-03-01 09:38:20 -0500

    Sh file test

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#
+#get the parameters
+
+par=$1
+result=$(racket "work-test-p"$par".rkt" 2>&1 > /dev/null)
+if [ -z "$result" ]
+then
+  echo "Test succeeded"
+else
+  echo "Test failed with error: "$result
+  exit 125
+fi
+
